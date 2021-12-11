b55b5ad3ce8c30f15e4153a28898f05d08ffd91d
Yun Hsieh <kilicapeto@gmail.com>
2021-12-11 21:50:19 +0800

    add: 0100 SameTree

---
+++ 0100.SameTree/SameTree.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    . "fmt"
+) 
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+    if p == nil && q == nil {
+        return true
+    } else if p== nil || q==nil {
+        return false
+    }
+    if p.Val == q.Val {
+        return isSameTree(p.Right, q.Right) && isSameTree(p.Left, q.Left)
+    } else {
+        return false
+    }
+}
+
+func main () {
+}
