bacef0caf2735d259424ddebbb0dad3b340a2683
Yun Hsieh <kilicapeto@gmail.com>
2021-12-11 15:36:52 +0800

    add: two sum

---
+++ 0001.TwoSum/TwoSum-map.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    . "fmt"
+) 
+
+func twoSum(nums []int, target int) []int {
+    mapping := map[int]int{}
+    for i, x := range nums {
+        if val, ok := mapping[target-x]; ok{
+            return []int{i, val}
+        }
+        mapping[x] = i
+    }
+        return []int{}
+}
+
+func main () {
+    a := twoSum([]int{1,3,4,5}, 4)
+    Println(a)
+
+}
