a5cd2757743ebac75a68e448a96837a03263b713
JerryHsieh <echoyun.hsieh@gmail.com>
2022-01-09 01:19:08 +0800

    add: 111 Minimum Depth of Binary tree

---
+++ 111.MinimumDepthofBinaryTree/MinimumDepthofBinaryTree.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	. "fmt"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	depth := 1
+	node := []*TreeNode{root}
+	for {
+		tempNode := []*TreeNode{root}
+		for _, v := range node {
+			if v.Right == nil && v.Left == nil {
+				return depth
+			}
+			if v.Right != nil {
+				tempNode = append(tempNode, v.Right)
+			}
+			if v.Left != nil {
+				tempNode = append(tempNode, v.Left)
+			}
+		}
+		depth++
+		node = tempNode
+	}
+	return depth
+}
