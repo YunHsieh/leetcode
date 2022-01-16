ed57a9b92c1700706e6bb2c755ecbdae4f5838ca
JerryHsieh <echoyun.hsieh@gmail.com>
2022-01-16 16:46:36 +0800

    add: 3Sum

---
+++ 15.3Sum/3Sum.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"sort"
+)
+
+func ThreeSum(nums []int) [][]int {
+    result := [][]int{}
+    sort.Ints(nums)
+    for i:=0; len(nums)-2>i; i++ {
+        if i!=0 && nums[i-1] == nums[i] {
+			continue
+		}
+        var start, end int = i+1, len(nums)-1
+        for ;start < end; {
+            ans := nums[i] + nums[start] + nums[end]
+            if ans > 0 {
+                end--
+            } else if ans < 0 {
+                start++
+            } else if ans == 0 {
+                newElement := []int{nums[i], nums[start], nums[end]}
+                nextElement := start 
+                result = append(result, newElement)
+                for nextElement < end && nums[start] == nums[nextElement] {
+                    nextElement++
+                }
+                start = nextElement
+            }
+        }
+    }
+    return result
+}
