1e955fa470e84dd8f6d49ce7e30118731ee49fda
JerryHsieh <echoyun.hsieh@gmail.com>
2022-01-21 01:17:05 +0800

    add: 0020.ValidParentheses

---
+++ 0020.ValidParentheses/ValidParentheses.go
@@ -0,0 +1,24 @@
+package main
+
+func IsValid(s string) bool {
+    symbols := map[byte]byte{
+        '(': ')',
+        '[': ']',
+        '{': '}',
+    }
+    stack := []byte{}
+    for i:=0; len(s)>i; i++ {
+        if val, ok := symbols[s[i]]; ok {
+            stack = append(stack, val)
+        } else {
+            if len(stack) == 0 || stack[len(stack)-1] != s[i] {
+                return false
+            }
+            stack = stack[:len(stack)-1]
+        }
+    }
+    if len(stack) != 0 {
+        return false
+    }
+    return true
+}
