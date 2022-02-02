7655f1ef7aa91942e20e0401b7ecde84181a7421
JerryHsieh <echoyun.hsieh@gmail.com>
2022-02-02 22:21:22 +0800

    add: 0219.ContainsNearbyDuplicate

---
+++ 0219.ContainsDuplicateII/ContainsDuplicateII.go
@@ -0,0 +1,12 @@
+package main
+
+func ContainsNearbyDuplicate(nums []int, k int) bool {
+    mapping := map[int]int{}
+    for i, val := range nums {
+        if _, ok := mapping[val]; ok && i - mapping[val] <= k {
+            return true
+        }
+        mapping[val] = i
+    }
+    return false
+}
