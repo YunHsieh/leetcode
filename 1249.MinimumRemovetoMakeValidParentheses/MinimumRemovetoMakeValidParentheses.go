f8dee39668f824200a477a517cb54bd9fae243f1
Jerry Hsieh <jerry.hsieh@swag.live>
2022-03-16 10:12:50 +0800

    add: MinimumRemovetoMakeValidParentheses

---
+++ 1249.MinimumRemovetoMakeValidParentheses/MinimumRemovetoMakeValidParentheses.go
@@ -0,0 +1,28 @@
+func minRemoveToMakeValid(s string) string {
+    results := strings.Split(s, "")
+    stack := []byte{}
+    for i:=0; i < len(results); i++ {
+        if s[i] == '(' {
+            stack = append(stack, s[i])
+        } else if s[i] == ')' {
+            if len(stack) != 0 {
+                stack = stack[:len(stack)-1]
+            } else {
+                results[i] = ""
+            }
+        }
+    }
+    if len(stack) != 0{
+        for i:=len(results)-1; i >= 0; i-- {
+            if len(stack) !=0 {
+                if results[i] == "(" {
+                    results[i] = ""
+                    stack = stack[:len(stack)-1]
+                }
+            } else {
+                break
+            }
+        }
+    }
+    return strings.Join(results, "")
+}
