3e759374c87e5086bfaf5cf85e0999468c6c8a2e
yun <kilicapeto@gmail.com>
2022-08-24 13:06:59 +0800

    add: 326.PowerofThree

---
+++ 326.PowerofThree/PowerofThree.go
@@ -0,0 +1,13 @@
+func isPowerOfThree(n int) bool {
+    if n == 0 {
+        return false
+    } else if n == 1{
+        return true
+    }
+    
+    if n := float64(n) / 3; n == float64(int(n)) {
+        return isPowerOfThree(int(n))
+    } else {
+        return false
+    }
+}
