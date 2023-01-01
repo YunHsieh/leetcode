e4c3deffd8b5e61c519f5670e19175c05ab84464
yun <kilicapeto@gmail.com>
2023-01-01 23:40:57 +0800

    add: 290.WordPattern

---
+++ 290.WordPattern/WordPattern.py
@@ -0,0 +1,11 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        s = s.split(" ")
+        if len(pattern) != len(s) or len(set(s)) != len(set(pattern)):
+            return False
+        mapping = {}
+        for i in range(len(s)):
+            mapping[pattern[i]] = mapping.get(pattern[i], s[i])
+            if mapping[pattern[i]] != s[i]:
+                return False
+        return True
