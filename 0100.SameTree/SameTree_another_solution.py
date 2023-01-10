cdb9a42b5548f33765968923e9feb9d142d6e9f2
Yun <52973591+YunHsieh@users.noreply.github.com>
2023-01-11 01:41:03 +0800

    add: 0100.SameTree (#94)

---
+++ 0100.SameTree/SameTree_another_solution.py
@@ -0,0 +1,16 @@
+"""
+Insteresting Solution
+"""
+
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        return hash(str(p)) == hash(str(q))
