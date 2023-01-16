aa3ec9f196b1accd9299797e6a37d10745624943
Yun <52973591+YunHsieh@users.noreply.github.com>
2023-01-16 16:28:34 +0800

    Feat/0057.insert interval (#98)
    * add: 0057.InsertInterval
    
    * add: comment

---
+++ 0057.InsertInterval/InsertInterval.py
@@ -0,0 +1,18 @@
+"""
+Runtime: O(N)
+Faster than: 94%
+"""
+
+class Solution:
+    def insert(self, intervals: list[list[int]], newInterval: list[int]) -> list[list[int]]:
+        intervals = intervals
+        start, end = newInterval
+        tmp = [start, end]
+        result = []
+        for a, b in intervals:
+            if a > end or b < start:
+                result.append([a, b])
+            else:
+                tmp.extend([a, b])
+        result.append([min(tmp), max(tmp)])
+        return sorted(result)
